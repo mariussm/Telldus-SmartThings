3a4628fa062b3abae7820efe2bc51d5e04a348a6
mariussm <marius.msm@gmail.com>
2016-12-06 20:05:38 +0100

    Working release

--- Telldus Connect SmartApp/Telldus Connect.groovy
+++ Telldus Connect SmartApp/Telldus Connect.groovy
@@ -18,7 +18,7 @@ definition(
 	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Partner/netamo-icon-1.png",
 	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Partner/netamo-icon-1%402x.png",
 	oauth: false,
-	singleInstance: false
+	singleInstance: true
 ){
 	appSetting "ApiUrl"
 	appSetting "ConsumerKey"
@@ -28,284 +28,10 @@ definition(
 }
 
 preferences {
-	//page(name: "Credentials", title: "Fetch OAuth2 Credentials", content: "authPage", install: false)
 	page(name: "listDevices", title: "Telldus Devices", content: "listDevices", install: false)
 }
 
 mappings {
-	//path("/oauth/initialize") {action: [GET: "oauthInitUrl"]}
-	//path("/oauth/callback") {action: [GET: "callback"]}
-}
-
-/*
-def authPage() {
-	log.debug "In authPage"
-
-	def description
-	def uninstallAllowed = false
-	def oauthTokenProvided = false
-
-	if (!state.accessToken) {
-		log.debug "About to create access token."
-		state.accessToken = createAccessToken()
-	}
-
-	if (canInstallLabs()) {
-
-		def redirectUrl = getBuildRedirectUrl()
-		// log.debug "Redirect url = ${redirectUrl}"
-
-		if (state.authToken) {
-			description = "Tap 'Next' to proceed"
-			uninstallAllowed = true
-			oauthTokenProvided = true
-		} else {
-			description = "Click to enter Credentials."
-		}
-
-		if (!oauthTokenProvided) {
-			log.debug "Show the login page"
-			return dynamicPage(name: "Credentials", title: "Authorize Connection", nextPage:"listDevices", uninstall: uninstallAllowed, install:false) {
-				section() {
-					paragraph "Tap below to log in to the netatmo and authorize SmartThings access."
-					href url:redirectUrl, style:"embedded", required:false, title:"Connect to ${getVendorName()}:", description:description
-				}
-			}
-		} else {
-			log.debug "Show the devices page"
-			return dynamicPage(name: "Credentials", title: "Credentials Accepted!", nextPage:"listDevices", uninstall: uninstallAllowed, install:false) {
-				section() {
-					input(name:"Devices", style:"embedded", required:false, title:"${getVendorName()} is now connected to SmartThings!", description:description) 
-				}
-			}
-		}
-	} else {
-		def upgradeNeeded = """To use SmartThings Labs, your Hub should be completely up to date.
-
-To update your Hub, access Location Settings in the Main Menu (tap the gear next to your location name), select your Hub, and choose "Update Hub"."""
-
-
-		return dynamicPage(name:"Credentials", title:"Upgrade needed!", nextPage:"", install:false, uninstall: true) {
-			section {
-				paragraph "$upgradeNeeded"
-			}
-		}
-
-	}
-}*/
-
-/*
-def oauthInitUrl() {
-	log.debug "In oauthInitUrl"
-
-	state.oauthInitState = UUID.randomUUID().toString()
-
-	def oauthParams = [
-		response_type: "code",
-		client_id: getClientId(),
-		client_secret: getClientSecret(),
-		state: state.oauthInitState,
-		redirect_uri: getCallbackUrl(),
-		scope: "read_station"
-	]
-
-	// log.debug "REDIRECT URL: ${getVendorAuthPath() + toQueryString(oauthParams)}"
-
-	redirect (location: getVendorAuthPath() + toQueryString(oauthParams))
-}
-*/
-/*
-def callback() {
-	// log.debug "callback()>> params: $params, params.code ${params.code}"
-
-	def code = params.code
-	def oauthState = params.state
-
-	if (oauthState == state.oauthInitState) {
-
-		def tokenParams = [
-			client_secret: getClientSecret(),
-			client_id : getClientId(),
-			grant_type: "authorization_code",
-			redirect_uri: getCallbackUrl(),
-			code: code,
-			scope: "read_station"
-		]
-
-		// log.debug "TOKEN URL: ${getVendorTokenPath() + toQueryString(tokenParams)}"
-
-		def tokenUrl = getVendorTokenPath()
-		def params = [
-			uri: tokenUrl,
-			contentType: 'application/x-www-form-urlencoded',
-			body: tokenParams
-		]
-
-		// log.debug "PARAMS: ${params}"
-
-		httpPost(params) { resp ->
-
-			def slurper = new JsonSlurper()
-
-			resp.data.each { key, value ->
-				def data = slurper.parseText(key)
-
-				state.refreshToken = data.refresh_token
-				state.authToken = data.access_token
-				state.tokenExpires = now() + (data.expires_in * 1000)
-				// log.debug "swapped token: $resp.data"
-			}
-		}
-
-		// Handle success and failure here, and render stuff accordingly
-		if (state.authToken) {
-			success()
-		} else {
-			fail()
-		}
-
-	} else {
-		log.error "callback() failed oauthState != state.oauthInitState"
-	}
-}
-*/
-/*
-def success() {
-	log.debug "in success"
-	def message = """
-	<p>We have located your Telldus account.</p>
-	<p>Tap 'Done' to continue to Devices.</p>
-	"""
-	connectionStatus(message)
-}
-
-def fail() {
-	log.debug "in fail"
-	def message = """
-	<p>The connection could not be established!</p>
-	<p>Click 'Done' to return to the menu.</p>
-	"""
-	connectionStatus(message)
-}
-
-def connectionStatus(message, redirectUrl = null) {
-	def redirectHtml = ""
-	if (redirectUrl) {
-		redirectHtml = """
-			<meta http-equiv="refresh" content="3; url=${redirectUrl}" />
-		"""
-	}
-
-	def html = """
-		<!DOCTYPE html>
-		<html>
-		<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>${getVendorName()} Connection</title>
-		<style type="text/css">
-			* { box-sizing: border-box; }
-			@font-face {
-				font-family: 'Swiss 721 W01 Thin';
-				src: url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-thin-webfont.eot');
-				src: url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-thin-webfont.eot?#iefix') format('embedded-opentype'),
-				url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-thin-webfont.woff') format('woff'),
-				url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-thin-webfont.ttf') format('truetype'),
-				url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-thin-webfont.svg#swis721_th_btthin') format('svg');
-				font-weight: normal;
-				font-style: normal;
-			}
-			@font-face {
-				font-family: 'Swiss 721 W01 Light';
-				src: url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-light-webfont.eot');
-				src: url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-light-webfont.eot?#iefix') format('embedded-opentype'),
-				url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-light-webfont.woff') format('woff'),
-				url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-light-webfont.ttf') format('truetype'),
-				url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-light-webfont.svg#swis721_lt_btlight') format('svg');
-				font-weight: normal;
-				font-style: normal;
-			}
-			.container {
-				width: 100%;
-				padding: 40px;
-				text-align: center;
-			}
-			img {
-				vertical-align: middle;
-			}
-			img:nth-child(2) {
-				margin: 0 30px;
-			}
-			p {
-				font-size: 2.2em;
-				font-family: 'Swiss 721 W01 Thin';
-				text-align: center;
-				color: #666666;
-				margin-bottom: 0;
-			}
-			span {
-				font-family: 'Swiss 721 W01 Light';
-				}
-		</style>
-		</head>
-		<body>
-			<div class="container">
-				<img src=""" + getVendorIcon() + """ alt="Vendor icon" />
-				<img src="https://s3.amazonaws.com/smartapp-icons/Partner/support/connected-device-icn%402x.png" alt="connected device icon" />
-				<img src="https://s3.amazonaws.com/smartapp-icons/Partner/support/st-logo%402x.png" alt="SmartThings logo" />
-				${message}
-			</div>
-        </body>
-        </html>
-	"""
-	render contentType: 'text/html', data: html
-}
-*/
-/*
-def refreshToken() {
-	log.debug "In refreshToken"
-
-	def oauthParams = [
-		client_secret: getClientSecret(),
-		client_id: getClientId(),
-		grant_type: "refresh_token",
-		refresh_token: state.refreshToken
-	]
-
-	def tokenUrl = getVendorTokenPath()
-	def params = [
-		uri: tokenUrl,
-		contentType: 'application/x-www-form-urlencoded',
-		body: oauthParams,
-	]
-
-	// OAuth Step 2: Request access token with our client Secret and OAuth "Code"
-	try {
-		httpPost(params) { response ->
-			def slurper = new JsonSlurper();
-
-			response.data.each {key, value ->
-				def data = slurper.parseText(key);
-				// log.debug "Data: $data"
-
-				state.refreshToken = data.refresh_token
-				state.accessToken = data.access_token
-				state.tokenExpires = now() + (data.expires_in * 1000)
-				return true
-			}
-
-		}
-	} catch (Exception e) {
-		log.debug "Error: $e"
-	}
-
-	// We didn't get an access token
-	if ( !state.accessToken ) {
-		return false
-	}
-}*/
-
-String toQueryString(Map m) {
-	return m.collect { k, v -> "${k}=${URLEncoder.encode(v.toString())}" }.sort().join("&")
 }
 
 def installed() {
@@ -324,37 +50,29 @@ def initialize() {
 	log.debug "Initialized with settings: ${settings}"
 
 	// Pull the latest device info into state
-	getDeviceList(); // TODO
+	getDeviceList();
 
 	settings.devices.each {
 		def deviceId = it
-		def detail = state?.deviceDetail[deviceId] // TODO
-
-		try {
-			switch(detail?.type) {
-				case 'sensor':
-					log.debug "type: sensor"
-					createChildDevice("Telldus Sensor", deviceId, detail.id, detail.name)
-                    // (deviceFile, dni, name, label)
-					break
-                /*
-				case 'NAModule1':
-					log.debug "Outdoor module"
-					createChildDevice("Netatmo Outdoor Module", deviceId, "${detail.type}.${deviceId}", detail.module_name)
-					break
-				case 'NAModule3':
-					log.debug "Rain Gauge"
-					createChildDevice("Netatmo Rain", deviceId, "${detail.type}.${deviceId}", detail.module_name)
-					break
-				case 'NAModule4':
-					log.debug "Additional module"
-					createChildDevice("Netatmo Additional Module", deviceId, "${detail.type}.${deviceId}", detail.module_name)
-					break
-				*/
-			}
-		} catch (Exception e) {
-			log.error "Error creating device: ${e}"
-		}
+        //log.debug "it: ${it}"
+        log.debug "foreach: ${deviceId}"
+		def detail = state.deviceDetail[deviceId] // TODO
+		log.debug "detail: ${detail}"
+        
+        if(detail != null) {
+            try {
+                switch(detail.type) {
+                    case 'sensor':
+                        log.debug "Calling createChildDevice: ${detail}"
+                        createChildDevice("Telldus Sensor", deviceId, detail.name, detail.id)
+                        break
+                    default:
+                        log.debug "Unknown type: ${detail.type}"
+                }
+            } catch (Exception e) {
+                log.error "Error creating device1: ${detail}: ${e}"
+            }
+        }
 	}
 
 	// Cleanup any other devices that need to go away
@@ -381,13 +99,16 @@ def getDeviceList() {
 	state.deviceState = [:]
 
 	apiGet("api/sensor") { response -> 
-        log.debug "response callback thingy"
+        log.debug "api/sensor response: ${response.data}"
         response.data.sensor.each { value ->
-            def key = value.id
+            def key = "telldus:${value.id}"
             log.debug "key: ${key}"
+            state.deviceDetail[key] = [:]
+            state.deviceDetail[key]["name"] = value.name
+            state.deviceDetail[key]["id"] = value.id
+            state.deviceDetail[key]["type"] = "sensor"
+            state.deviceState[key] = null
             deviceList[key] = "${value.name}"
-            state.deviceDetail[key] = value
-            state.deviceState[key] = value.name
         }
 
 	}
@@ -396,25 +117,25 @@ def getDeviceList() {
 }
 
 private removeChildDevices(delete) {
-	log.debug "In removeChildDevices"
-	log.debug "deleting ${delete.size()} devices"
+	log.debug "In removeChildDevices - deleting ${delete.size()} devices"
 	delete.each {
 		deleteChildDevice(it.deviceNetworkId)
 	}
 }
 
-def createChildDevice(deviceFile, dni, name, label) {
+def createChildDevice(deviceFile, dni, name, telldusId) {
 	log.debug "In createChildDevice"
-	try {
+	
+    try {
 		def existingDevice = getChildDevice(dni)
 		if(!existingDevice) {
 			log.debug "Creating child"
-			def childDevice = addChildDevice("dianoga", deviceFile, dni, null, [name: name, label: label, completedSetup: true]) // TODO??
+			def childDevice = addChildDevice("mariussm", deviceFile, dni, null, [name: name, telldusId: telldusId])
 		} else {
 			log.debug "Device $dni already exists"
 		}
 	} catch (e) {
-		log.error "Error creating device: ${e}"
+		log.error "Error creating device2: ${e}"
 	}
 }
 
@@ -428,9 +149,9 @@ def listDevices() {
 			input "devices", "enum", title: "Select Device(s)", required: false, multiple: true, options: devices
 		}
 
-        section("Preferences") {
+        /*section("Preferences") {
         	input "rainUnits", "enum", title: "Rain Units", description: "Millimeters (mm) or Inches (in)", required: true, options: [mm:'Millimeters', in:'Inches']
-        }
+        }*/
 	}
 }
 
@@ -462,46 +183,65 @@ def apiGet(String path, Closure callback) {
 
 def poll() {
 	log.debug "In Poll"
-	// TODO: getDeviceList();
-	def children = getChildDevices()
+    
+    def children = getChildDevices()
     log.debug "State: ${state.deviceState}"
 
 	settings.devices.each { deviceId ->
 		def detail = state?.deviceDetail[deviceId]
-		def data = state?.deviceState[deviceId]
-		def child = children?.find { it.deviceNetworkId == deviceId }
+        if(detail != null) {
+            log.debug "poll detail: ${detail}"
 
-		log.debug "Update: $child";
-		switch(detail?.type) {
-			case 'sensor':
-				log.debug "Updating sensor $data"
-				child?.sendEvent(name: 'temperature', value: cToPref(data['Temperature']) as float, unit: getTemperatureScale())
-				//child?.sendEvent(name: 'carbonDioxide', value: data['CO2'])
-				child?.sendEvent(name: 'humidity', value: data['Humidity'])
-				//child?.sendEvent(name: 'pressure', value: data['Pressure'])
-				//child?.sendEvent(name: 'noise', value: data['Noise'])
-				break;
-			/*case 'NAModule1':
-				log.debug "Updating NAModule1 $data"
-				child?.sendEvent(name: 'temperature', value: cToPref(data['Temperature']) as float, unit: getTemperatureScale())
-				child?.sendEvent(name: 'humidity', value: data['Humidity'])
-				break;
-			case 'NAModule3':
-				log.debug "Updating NAModule3 $data"
-				child?.sendEvent(name: 'rain', value: rainToPref(data['Rain']) as float, unit: settings.rainUnits)
-				child?.sendEvent(name: 'rainSumHour', value: rainToPref(data['sum_rain_1']) as float, unit: settings.rainUnits)
-				child?.sendEvent(name: 'rainSumDay', value: rainToPref(data['sum_rain_24']) as float, unit: settings.rainUnits)
-				child?.sendEvent(name: 'units', value: settings.rainUnits)
-				break;
-			case 'NAModule4':
-				log.debug "Updating NAModule4 $data"
-				child?.sendEvent(name: 'temperature', value: cToPref(data['Temperature']) as float, unit: getTemperatureScale())
-				child?.sendEvent(name: 'carbonDioxide', value: data['CO2'])
-				child?.sendEvent(name: 'humidity', value: data['Humidity'])
-				break;
-			*/
-		}
+            def data = [:]
+            data['Temperature'] = null
+            data['Humidity'] = null
+
+            def httpparams = [:]
+            httpparams['Id'] = detail.id
+            apiGet("api/sensordata", httpparams) { response -> 
+                log.debug "api/sensordata response data: ${response.data}"
+                response.data.data.each { value ->
+                    log.debug "value: ${value}"
+                    if(value.name == "humidity") {
+                        data['Humidity'] = value.value
+                    }
+                    if(value.name == "temp") {
+                        data['Temperature'] = value.value
+                    }
+                }
+            /*response.data.sensor.each { value ->
+                def key = "telldus:${value.id}"
+                log.debug "key: ${key}"
+                state.deviceDetail[key] = [:]
+                state.deviceDetail[key]["name"] = value.name
+                state.deviceDetail[key]["id"] = value.id
+                state.deviceDetail[key]["type"] = "sensor"
+                state.deviceState[key] = null
+                deviceList[key] = "${value.name}"
+            */
+            }
+
+
+
+            //def data = state?.deviceState[deviceId] // TODO
+
+            def child = children?.find { it.deviceNetworkId == deviceId }
+
+            log.debug "Update: $child";
+            switch(detail?.type) {
+                case 'sensor':
+                    log.debug "Updating sensor $data"
+                    child?.sendEvent(name: 'temperature', value: cToPref(data['Temperature']) as float, unit: getTemperatureScale())
+                    //child?.sendEvent(name: 'carbonDioxide', value: data['CO2'])
+                    child?.sendEvent(name: 'humidity', value: data['Humidity'])
+                    //child?.sendEvent(name: 'pressure', value: data['Pressure'])
+                    //child?.sendEvent(name: 'noise', value: data['Noise'])
+                    break;
+            }
+        }
 	}
+    
+    
 }
 
 def cToPref(temp) {
